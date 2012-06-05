06a77d65d9cec5b12b3f410b392a78a95adb452b
Draco Li <draco@dracoli.com>
2012-06-04 23:36:11 -0400

    added backend project

---
+++ DBackend/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DBackend::Application.config.secret_token = 'fc9f2446e16aa4e2554b942f79776800c8b1ae7702008bb82a64ae9b1f8d91da45a922d74fcbd0d1fc7e40f4a4ca7eb1dff23c5015f7b065ff769f906f83054c'
